c22475e22c4539d2491d6ca5aaa6ea1704a74386
Corey-CBC-Fen <coreyfenhaus@sheridan.edu>
2025-04-10 14:57:07 +0000

    episode 4 ? be3ginning

---
+++ python-learning/networkChuck/list.py
@@ -0,0 +1,8 @@
+campStuff = "tent, sleeping bags, water, guns, coffee, knives, lighters"
+
+
+campingList = ["tent, sleeping bags, water, guns, coffee, knives, lighters"]
+
+print(campStuff)
+
+print(type(campingList))
